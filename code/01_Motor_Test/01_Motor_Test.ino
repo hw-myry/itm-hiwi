40ddb565ef190ebf4374df8aab41e8ab121c7461
myry <jiaqi.wang@tu-braunschweig.de>
2026-06-09 22:01:18 +0200

    add test file

--- code/01_Motor_Test/01_Motor_Test.ino
+++ code/01_Motor_Test/01_Motor_Test.ino
@@ -1,33 +1,28 @@
 #define STEP_PIN 2
 #define DIR_PIN  1
 
+#define STEPS_PER_REV 3200   // 1600/3200/6400
+
 void setup() {
   pinMode(STEP_PIN, OUTPUT);
   pinMode(DIR_PIN, OUTPUT);
+}
 
-  digitalWrite(DIR_PIN, HIGH);
+void stepMotor(int steps, bool dir) {
+  digitalWrite(DIR_PIN, dir);
+
+  for (int i = 0; i < steps; i++) {
+    digitalWrite(STEP_PIN, HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(STEP_PIN, LOW);
+    delayMicroseconds(1000);
+  }
 }
 
 void loop() {
-  // 正转一圈：1.8°电机通常 200 步/圈
-  digitalWrite(DIR_PIN, HIGH);
-  for (int i = 0; i < 200; i++) {
-    digitalWrite(STEP_PIN, HIGH);
-    delayMicroseconds(1000);
-    digitalWrite(STEP_PIN, LOW);
-    delayMicroseconds(1000);
-  }
-
+  stepMotor(STEPS_PER_REV, HIGH);
   delay(1000);
 
-  // 反转一圈
-  digitalWrite(DIR_PIN, LOW);
-  for (int i = 0; i < 200; i++) {
-    digitalWrite(STEP_PIN, HIGH);
-    delayMicroseconds(1000);
-    digitalWrite(STEP_PIN, LOW);
-    delayMicroseconds(1000);
-  }
-
+  stepMotor(STEPS_PER_REV, LOW);
   delay(1000);
 }
